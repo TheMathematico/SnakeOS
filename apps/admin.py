f6bd76fbc2c230e86a10cc656a59ad812656de99
matest <matestef191@gmail.com>
2026-08-28 19:19:15 +0200

    you can now create as many assignables as you want, for now there are clicks and clicks_add

--- apps/admin.py
+++ apps/admin.py
@@ -10,7 +10,7 @@ vertical_offset = 5
 assignable_height = 50
 assignable_width = w - horizontal_offset * 2
 
-def create_assignable(parent, variable, name, fg, rainbow = False):
+def create_assignable(parent, variable, module, name, fg, rainbow = False):
     print("called")
     global assignable_height, assignable_width, horizontal_offset, vertical_offset
 
@@ -23,14 +23,46 @@ def create_assignable(parent, variable, name, fg, rainbow = False):
     title = ctk.CTkLabel(frame, text=f"{name}, type: {variable_type}", font=("monogram", 32))
     title.pack(padx=horizontal_offset, pady=0, side="left")
 
-    submit_button = ctk.CTkButton(frame, width=50, height=45, text="✔", font=("monogram", 32), fg_color="green")
+    def submit():
+        nonlocal entry, submit_button
+        nonlocal variable, variable_type
+
+        try:
+            print('yes')
+
+            if variable_type != "int":
+                print("banan")
+                change = entry.get()
+            else:
+                print("yabloko")
+                change = int(entry.get())
+
+            setattr(module, name, change)
+            submit_button.configure(fg_color="lime")
+            parent.after(250, submit_button.configure(fg_color="green"))
+        except Exception:
+            print('bruh')
+            submit_button.configure(fg_color="red")
+            parent.after(250, submit_button.configure(fg_color="green"))
+
+
+    submit_button = ctk.CTkButton(
+        frame, 
+        width=50, 
+        height=45, 
+        text="✔", 
+        font=("monogram", 32), 
+        fg_color="green",
+        hover_color=utility.BACKGROUND_HOVER,
+        command=submit
+    )
     submit_button.pack(padx=horizontal_offset, pady=0, side="right")
 
     entry = ctk.CTkEntry(
         frame, 
         width=int(assignable_width/3),
         height=assignable_height-vertical_offset,
-        placeholder_text=f"Set {name}"
+        placeholder_text=f"Set {name}",
     )
     entry.pack(padx=0, pady=0, side="right") #0 padx because submit_button already has the offset both left and right
 
@@ -44,4 +76,6 @@ def open(clicker):
     title = ctk.CTkLabel(window, text="Evil cheatsheet >:(", font=("monogram", 32))
     title.pack(padx=0, pady=0, anchor="n")
 
-    create_assignable(window, clicker.clicks, "clicks", utility.BACKGROUND_SECONDARY)
+    create_assignable(window, clicker.clicks, clicker, "clicks", utility.BACKGROUND_SECONDARY)
+    create_assignable(window, clicker.clicks, clicker, "clicks_add", utility.BACKGROUND_SECONDARY)
+    create_assignable(window, clicker.clicks, clicker, "income_delay", utility.BACKGROUND_SECONDARY)
