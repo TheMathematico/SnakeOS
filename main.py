a92facadb1490a4d406113e807d70e25d87093ed
matest <matestef191@gmail.com>
2026-09-01 18:33:40 +0200

    forgot to add main.py to my last commit, i added utility.create_app for the note app

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import customtkinter as ctk
 import utility
-from apps import time
-from apps import clicker
+import time
+from apps import time_app, clicker, admin, notes
 from PIL import Image
 import pygame #for sound
 
@@ -16,10 +16,11 @@ TOP_FRAME_HEIGHT = 30
 
 photo = Image.open("appdefault.png")
 logo = Image.open("logo.png")
-print(logo)
 clock_icon = Image.open("clock.png")
 cursor = Image.open("cursor.png")
 clicker_icon = Image.open("clicker.png")
+admin_icon = Image.open("admin.png")
+folder_icon = Image.open("folder.png")
 
 BACKGROUND = "#0B1F1A"
 BACKGROUND_SECONDARY = "#14332A"
@@ -47,7 +48,9 @@ taskbar.pack_propagate(False)
 
 workspace = ctk.CTkLabel(root, width=WIDTH-tsk_bar_width-30, height=HEIGHT-20, fg_color=BACKGROUND_SECONDARY, corner_radius=15, text="", image=background)
 workspace.place(x=tsk_bar_width + 20, y=10)
-
+workspace.update_idletasks() #This line is here so that ctk calculates the geometry of workspace(since for some reason it doesn't automatically, at least quick enough)
+                             #because create_app needs the height of workspace so that it can
+                             #organize the apps properly
 utility.workspace = workspace
 utility.root = root
 utility.TOP_FRAME_HEIGHT = TOP_FRAME_HEIGHT
@@ -58,9 +61,10 @@ utility.BACKGROUND_SECONDARY = BACKGROUND_SECONDARY
 close = utility.add_button(taskbar, 80, tsk_bar_width-4, "shutdown", 22, on_close, BACKGROUND_SECONDARY, BACKGROUND_HOVER, 10, "bottom")
 asjdlasd = utility.add_button(taskbar, 80, tsk_bar_width-4, "+", 32, lambda: utility.create_window(300, 300, "test", BACKGROUND_HOVER), BACKGROUND_SECONDARY, BACKGROUND_HOVER, 0, "bottom")
 
-utility.create_app("time", clock_icon, lambda event: time.open())
-utility.create_app("ZooClicker", clicker_icon, lambda event: clicker.open(cursor, BACKGROUND_SECONDARY))
-utility.create_app("uno", photo, lambda event: utility.create_window(500, 500, "uno"))
+utility.create_app("Time", clock_icon, lambda event: time_app.open(utility, ctk, time))
+utility.create_app("ZooClicker", clicker_icon, lambda event: clicker.open(cursor, BACKGROUND_SECONDARY, ctk, utility))
+utility.create_app("Admin", admin_icon, lambda event: admin.open(clicker, ctk, utility))
+utility.create_app("Notes", admin_icon, lambda event: notes.open(ctk, utility))
 
 utility.update_loop()
 
